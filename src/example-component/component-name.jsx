9155d70d9ce478127054f06d324fd3b0027122de
Swatipatel2421 <115700974+Swatipatel2421@users.noreply.github.com>
2023-03-23 15:42:44 +0530

    add code

--- src/example-component/component-name.jsx
+++ src/example-component/component-name.jsx
@@ -9,7 +9,7 @@ class ComponentName extends Component {
                 {/* <div className="container">
                     <div className="row"> */}
                         <div className="col-md-6 col-12 border border-bottom-0 border-start-0 border-warning pt-3">
-                            <Link to="classcomponent" className="text-center fs-5 fw-bold text-dark text-opacity-75">Class Component</Link>
+                            <Link to="classcomponent" className="text-center fs-5 fw-bold text-dark text-opacity-75">Class Components</Link>
                         </div>
                         <div className="col-md-6 col-12 border border-bottom-0 border-end-0 border-warning pt-3">
                             <Link to="functionComponent" className="text-center fs-5 fw-bold text-dark text-opacity-75">Functional Component</Link>
